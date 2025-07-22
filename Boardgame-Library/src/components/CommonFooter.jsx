28653252d51b1f2529a7fb321dcc10bb673e9af2
cdbroyles <cdbroyles1986@gmail.com>
2025-07-22 18:05:40 -0500

    Added a homepage link to my footer

--- Boardgame-Library/src/components/CommonFooter.jsx
+++ Boardgame-Library/src/components/CommonFooter.jsx
@@ -4,6 +4,7 @@ const Footer = () => {
     return (
         <footer>
             <h3>&copy; 2025 Boardgame Library. All rights reserved.</h3>
+            <Link to="/" className="footer-link">Home Page</Link>
             <Link to="/About" className="footer-link">About Page</Link>
         </footer>
     );
