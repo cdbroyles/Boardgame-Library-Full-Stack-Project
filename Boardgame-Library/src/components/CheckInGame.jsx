9ece2f2f1f8420d23f89b61a50e0cc94e48b6cc7
cdbroyles <cdbroyles1986@gmail.com>
2025-07-24 20:17:45 -0500

    fixed deployment image error

--- Boardgame-Library/src/components/CheckInGame.jsx
+++ Boardgame-Library/src/components/CheckInGame.jsx
@@ -1,3 +1,5 @@
+import RemoveFromCartIcon from '../assets/RemoveFromCartIcon.png';
+
 const CheckInGame = (prop) => {
     //code to differentiate class name for when item is available vs. when item is not available
     const iconClass = !prop.isAvailable ? "shopping-cart-icon" : "inactive";
@@ -5,7 +7,7 @@ const CheckInGame = (prop) => {
     return (
     //check in game image with variable class name.  Also has logic for disabling check in when the game is already available.
         <img 
-            src="src/assets/RemoveFromCartIcon.png"
+            src={RemoveFromCartIcon}
             onClick={() => {!prop.isAvailable ? prop.processCheckIn(prop.game) : <></>}} 
             alt="Remove from Cart" 
             id="remove-from-cart-icon" 
