40e9093c717d134db4f79bf5d87a5f177b63308e
cdbroyles <cdbroyles1986@gmail.com>
2025-07-24 20:10:22 -0500

    deployment error check #4

--- Boardgame-Library/src/components/CheckoutGame.jsx
+++ Boardgame-Library/src/components/CheckoutGame.jsx
@@ -1,3 +1,5 @@
+import RemoveFromCartIcon from '../assets/RemoveFromCartIcon.png';
+
 const CheckOutGame = (prop) => {
     //code to differentiate class name for when item is available vs. when item is not available
     const iconClass = prop.isAvailable ? "shopping-cart-icon" : "inactive";
@@ -5,7 +7,7 @@ const CheckOutGame = (prop) => {
     return (
         //check out game image with variable class name.  Also has logic for disabling check out when the game is not available.
         <img 
-            src="assets/AddToCartIcon.png" 
+            src={RemoveFromCartIcon}
             onClick={(event) => {prop.isAvailable ? prop.processCheckOut(event) : <></>}}
             alt="Add to Cart" 
             id="add-to-cart-icon"
