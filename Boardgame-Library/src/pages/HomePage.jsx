0ffda18e9b1cf3722b283a460a9407a0658c6a3e
cdbroyles <cdbroyles1986@gmail.com>
2025-09-20 10:39:43 -0500

    Added login page (no body) along with login state variable

--- Boardgame-Library/src/pages/HomePage.jsx
+++ Boardgame-Library/src/pages/HomePage.jsx
@@ -1,14 +1,18 @@
 import Footer from "../components/CommonFooter";
 import Header from "../components/CommonHeader";
+import LogInButton from "../components/LogInButton";
 import ViewCheckedOutItemsButton from "../components/ViewCheckedOutItemsButton";
 import ViewGameLibraryButton from "../components/ViewGameLibraryButton";
+import {useState, useEffect} from "react";
 
 const HomePage = () => {
+    const [isLogIn, setIsLogIn] = useState(false);
+
     return(
         <div className="fill-page">
             <Header />
             <main id="flexbox-home" className="body-content">
-                <ViewCheckedOutItemsButton />
+                {isLogIn ? <ViewCheckedOutItemsButton /> : <LogInButton />}
                 <ViewGameLibraryButton />
             </main>
             <Footer />
---
+++ Boardgame-Library/src/pages/LogInPage.jsx
@@ -0,0 +1,16 @@
+import Footer from "../components/CommonFooter";
+import Header from "../components/CommonHeader";
+
+const LogInPage = () => {
+    return(
+        <div className="fill-page">
+            <Header />
+            <main id="flexbox-home" className="body-content">
+                
+            </main>
+            <Footer />
+        </div>
+    )
+}
+
+export default LogInPage;
