1db36dfc221df1614a89e5467c792c86ad93c39e
cdbroyles <cdbroyles1986@gmail.com>
2025-10-06 17:54:55 -0500

    Have basic structure of Personel page with common header and footer.

---
+++ Boardgame-Library/src/pages/UpdatePersonel.jsx
@@ -0,0 +1,17 @@
+import Footer from "../components/CommonFooter";
+import Header from "../components/CommonHeader";
+
+const UpdatePersonel = () => {
+
+    return (
+        <div className="fill-page">
+            <Header />
+            <main>
+
+            </main>
+            <Footer />
+        </div>
+    )
+}
+
+export default UpdatePersonel;
