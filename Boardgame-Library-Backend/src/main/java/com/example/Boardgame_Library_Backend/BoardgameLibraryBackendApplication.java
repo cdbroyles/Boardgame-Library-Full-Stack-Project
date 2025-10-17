955179ce2991e1522c135317e3f7212da392f664
cdbroyles <cdbroyles1986@gmail.com>
2025-10-17 10:53:54 -0500

    spring boot initializr

---
+++ Boardgame-Library-Backend/src/main/java/com/example/Boardgame_Library_Backend/BoardgameLibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Boardgame_Library_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardgameLibraryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardgameLibraryBackendApplication.class, args);
+	}
+
+}
