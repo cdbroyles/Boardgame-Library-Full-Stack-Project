6019837ed88472d547e3906d0ab01fcdda8da3db
cdbroyles <cdbroyles1986@gmail.com>
2025-10-20 10:43:42 -0500

    finished adding user model information

--- Boardgame-Library-Backend/src/main/java/com/example/Boardgame_Library_Backend/Models/User.java
+++ Boardgame-Library-Backend/src/main/java/com/example/Boardgame_Library_Backend/Models/User.java
@@ -19,4 +19,10 @@ public class User {
     private String username;
     private String password;
     private boolean isAdmin;
+
+    public User(String username, String password, boolean isAdmin) {
+        this.username = username;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
 }
